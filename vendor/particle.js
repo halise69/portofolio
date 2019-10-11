26dbe166a6cad8b1fa2995f60d3f8fb5ff880bbf
halise69 <hhalise69@gmail.com>
2019-10-11 18:34:58 +0700

    oke

---
+++ vendor/particle.js
@@ -0,0 +1,9 @@
+particlesJS.load('particles-js', 'vendor/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles-js2', 'vendor/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles-js3', 'vendor/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
